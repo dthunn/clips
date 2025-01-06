ca4a10b0577243ebd8e8c33d3e3c2074c37e893e
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-01-06 13:50:45 -0700

    login/register form

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { provideNgxMask, NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgxMaskDirective],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrl: './input.component.scss',
+  providers: [provideNgxMask()],
 })
 export class InputComponent {
-
+  control = input.required<FormControl>();
+  type = input('text');
+  placeholder = input('');
+  format = input('');
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from '../../shared/input/input.component';
+import { AlertComponent } from '../../shared/alert/alert.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, InputComponent, AlertComponent],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  fb = inject(FormBuilder);
 
+  form = this.fb.nonNullable.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    age: [18, [Validators.required, Validators.min(18), Validators.max(120)]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+        ),
+      ],
+    ],
+    confirmPassword: ['', [Validators.required]],
+    phoneNumber: [
+      '',
+      [Validators.required, Validators.minLength(13), Validators.maxLength(13)],
+    ],
+  });
+
+  showAlert = signal(false);
+  alertMsg = signal('Please wait! Your account is being created.');
+  alertColor = signal('blue');
+
+  register() {
+    this.showAlert.set(true);
+    this.alertMsg.set('Please wait! Your account is being created.');
+    this.alertColor.set('blue');
+  }
 }
