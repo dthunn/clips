09bf35ffddadb1561f1eae3765fdc11bd6693e00
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-01-09 17:16:44 -0700

    firebase login functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './core/nav/nav.component';
 import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
+import { AuthService } from './services/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, AuthModalComponent],
+  imports: [RouterOutlet, NavComponent, AuthModalComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'clips';
+  auth = inject(AuthService);
 }
